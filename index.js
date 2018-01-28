748f697a1ea0945d17c8f9ee31c15b47d0fcfe85
Jennifer Tsai <jsyt.kai@gmail.com>
2018-01-28 23:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function theBeatlesPlay(musicians, instruments) {
   const theBeatles = [];
-  for(var i = 0; i < musicians.length; i++) {
+  for(let i = 0; i < musicians.length; i++) {
     theBeatles.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return theBeatles;
 }
 
 function johnLennonFacts(facts) {
-  var i = 0;
+  let i = 0;
   while (i < facts.length) {
     facts[i] = facts[i] + "!!!";
     i = i + 1;
